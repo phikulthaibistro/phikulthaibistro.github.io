01c3b79cf1e445bc11991ccc7fbe9171ea032b98
Keen Wilson <kwilson@finsignum.com>
2020-07-15 16:08:43 -0500

    Update from 15

---
+++ precache-manifest.407cd7e5c551db3cb67de85a55edde3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9047dbea2087d658dab19bbd2a0b985",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e34341a3c12f2ac7ab0",
+    "url": "/static/css/main.a8e2df81.chunk.css"
+  },
+  {
+    "revision": "71bd284a168bb6c7a9f0",
+    "url": "/static/js/2.1056f64d.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/static/js/2.1056f64d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e34341a3c12f2ac7ab0",
+    "url": "/static/js/main.a8096cde.chunk.js"
+  },
+  {
+    "revision": "e8dd683de585cbe77841",
+    "url": "/static/js/runtime-main.cf719910.js"
+  }
+]);
