a3476556373ae72585b0de12fcc752affc4f3dbb
Keen <kwilson@finsignum.com>
2020-07-15 10:20:05 -0500

    Update directory with first version of the website

---
+++ precache-manifest.ea8d60b00f26846ff649a2e7676e0a8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384ac095461a5914eef97c554aa12eff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e97550f1aaa856e6a540",
+    "url": "/static/css/main.a8e2df81.chunk.css"
+  },
+  {
+    "revision": "71bd284a168bb6c7a9f0",
+    "url": "/static/js/2.1056f64d.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/static/js/2.1056f64d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e97550f1aaa856e6a540",
+    "url": "/static/js/main.21aa1227.chunk.js"
+  },
+  {
+    "revision": "e8dd683de585cbe77841",
+    "url": "/static/js/runtime-main.cf719910.js"
+  }
+]);
