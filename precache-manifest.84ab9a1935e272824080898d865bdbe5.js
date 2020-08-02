8dcfeceba92c38ac85a061971a6329e21deff976
Keen <keen.wilson@optimally.com>
2020-08-02 18:22:40 -0500

    Update phone number and use one menu

---
+++ precache-manifest.84ab9a1935e272824080898d865bdbe5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cebc20a1485c2262d5a0604f3b6cab51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1993f2728b181a1c05ac",
+    "url": "/static/css/main.372d8c52.chunk.css"
+  },
+  {
+    "revision": "927004ae4b49b0981a5a",
+    "url": "/static/js/2.65a95baf.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/static/js/2.65a95baf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1993f2728b181a1c05ac",
+    "url": "/static/js/main.5279b93b.chunk.js"
+  },
+  {
+    "revision": "e8dd683de585cbe77841",
+    "url": "/static/js/runtime-main.cf719910.js"
+  },
+  {
+    "revision": "f83c8e55f63016c19a6baa993ad4ae1e",
+    "url": "/static/media/header-3dishes.f83c8e55.jpg"
+  },
+  {
+    "revision": "75a3fae5c0778c060738012028365a3a",
+    "url": "/static/media/header-menu.75a3fae5.png"
+  },
+  {
+    "revision": "0b7259e1926ea5c07c98076fcae2397b",
+    "url": "/static/media/header-thailand-icons.0b7259e1.png"
+  },
+  {
+    "revision": "6fec814da8b0424be9fbfd6a47e9187a",
+    "url": "/static/media/thailand-icons.6fec814d.png"
+  }
+]);
