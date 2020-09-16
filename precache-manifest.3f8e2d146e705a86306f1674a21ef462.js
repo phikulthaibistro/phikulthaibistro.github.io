80f059d533cd81cae04331ca05f62b772daa1ca5
Keen <keen.wilson@optimally.com>
2020-09-16 15:57:02 -0500

    Add option to download PDF on mobile

---
+++ precache-manifest.3f8e2d146e705a86306f1674a21ef462.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e196b6fc76c59374247a9ab8f0507ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f90947acbdf87575e153",
+    "url": "/static/css/main.6ae4eb91.chunk.css"
+  },
+  {
+    "revision": "34c73f708508f3f6cbdf",
+    "url": "/static/js/2.96619651.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.96619651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f90947acbdf87575e153",
+    "url": "/static/js/main.a705bf0a.chunk.js"
+  },
+  {
+    "revision": "a28e291ceb1b4f041c2c",
+    "url": "/static/js/runtime-main.15372b6c.js"
+  },
+  {
+    "revision": "5dc10a80dbca905d07e3a1cfc764d1d7",
+    "url": "/static/media/hero_phikulthai_1.5dc10a80.jpg"
+  },
+  {
+    "revision": "4106ab7949aaadea76d680c0bf07af1a",
+    "url": "/static/media/hero_phikulthai_2.4106ab79.jpg"
+  },
+  {
+    "revision": "82235a85a439fac39fea85f0a667cbf0",
+    "url": "/static/media/hero_phikulthai_mobile_1.82235a85.jpg"
+  },
+  {
+    "revision": "4f88cbf5fdd0330d42820fe508ec9b5d",
+    "url": "/static/media/phikul_icon.4f88cbf5.png"
+  },
+  {
+    "revision": "172713a530abe034aa5951095c9b0e2d",
+    "url": "/static/media/phikulthaibistro_logo.172713a5.png"
+  }
+]);
