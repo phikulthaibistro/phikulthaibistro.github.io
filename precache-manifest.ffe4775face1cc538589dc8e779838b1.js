cf5faceca33c4e5ee241975b64690c5e44befa9f
Keen <keencwilson@gmail.com>
2021-02-17 15:28:55 -0800

    Added ORDER ONLINE buttons

---
+++ precache-manifest.ffe4775face1cc538589dc8e779838b1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d344986dee46926f4c2af7eb01ed6b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3773bed26b51508981ae",
+    "url": "/static/css/main.0f9d09e2.chunk.css"
+  },
+  {
+    "revision": "90a63ceb5c6ddf81e095",
+    "url": "/static/js/2.ea4e74b0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ea4e74b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3773bed26b51508981ae",
+    "url": "/static/js/main.7547b92d.chunk.js"
+  },
+  {
+    "revision": "a28e291ceb1b4f041c2c",
+    "url": "/static/js/runtime-main.15372b6c.js"
+  },
+  {
+    "revision": "5dc10a80dbca905d07e3a1cfc764d1d7",
+    "url": "/static/media/hero_phikulthai_1.5dc10a80.jpg"
+  },
+  {
+    "revision": "4106ab7949aaadea76d680c0bf07af1a",
+    "url": "/static/media/hero_phikulthai_2.4106ab79.jpg"
+  },
+  {
+    "revision": "82235a85a439fac39fea85f0a667cbf0",
+    "url": "/static/media/hero_phikulthai_mobile_1.82235a85.jpg"
+  },
+  {
+    "revision": "4f88cbf5fdd0330d42820fe508ec9b5d",
+    "url": "/static/media/phikul_icon.4f88cbf5.png"
+  },
+  {
+    "revision": "172713a530abe034aa5951095c9b0e2d",
+    "url": "/static/media/phikulthaibistro_logo.172713a5.png"
+  }
+]);
