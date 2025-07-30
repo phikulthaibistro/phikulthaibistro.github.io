51fece9f0995062ca5e8007e29b29d33f1b2fe52
Keen <keencwilson@gmail.com>
2025-07-29 19:28:08 -0700

    Deploy updated website with new menu functionality and responsive navigation

---
+++ precache-manifest.730c1bbb29c7b0357de57a6cbbc0ecbf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbc54d2e824da1928adfc37a0bcada15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b516ba3cd5e733f0d724",
+    "url": "/static/css/main.7dfd8267.chunk.css"
+  },
+  {
+    "revision": "3dfea49cc9257667c492",
+    "url": "/static/js/2.66453ca9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.66453ca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b516ba3cd5e733f0d724",
+    "url": "/static/js/main.d1f1354d.chunk.js"
+  },
+  {
+    "revision": "a28e291ceb1b4f041c2c",
+    "url": "/static/js/runtime-main.15372b6c.js"
+  },
+  {
+    "revision": "5dc10a80dbca905d07e3a1cfc764d1d7",
+    "url": "/static/media/hero_phikulthai_1.5dc10a80.jpg"
+  },
+  {
+    "revision": "4106ab7949aaadea76d680c0bf07af1a",
+    "url": "/static/media/hero_phikulthai_2.4106ab79.jpg"
+  },
+  {
+    "revision": "82235a85a439fac39fea85f0a667cbf0",
+    "url": "/static/media/hero_phikulthai_mobile_1.82235a85.jpg"
+  },
+  {
+    "revision": "172713a530abe034aa5951095c9b0e2d",
+    "url": "/static/media/phikulthaibistro_logo.172713a5.png"
+  }
+]);
